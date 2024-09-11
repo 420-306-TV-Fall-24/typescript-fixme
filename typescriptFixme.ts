2cc7eaa93e57de6094e717a212e49bd8a124607f
Gagan <sandhugagandeep0601@gmail.com>
2024-09-11 19:15:27 -0400

    made the typscript changes

--- typescriptFixme.ts
+++ typescriptFixme.ts
@@ -1,41 +1,50 @@
 // add the type annotation to explicitly specify the type of the variable:
-var isDone = false;
-var lines = 42;
-var username = "Anders";
+var isDone: boolean = false;
+var lines: number = 42;
+var username:string = "Anders";
 
 // annotate the type as when it's not known
-let unknownTypeVariable;
+let unknownTypeVariable: unknown;
 
 //annotate this array
-var list = [1, 2, 3];
+var list:number[] = [1, 2, 3];
 
 // annotate the return type of this function
-function littleHorribleAlert() {
+function littleHorribleAlert():void {
   alert("I am a little annoying box!!");
 }
 
-var f1 = function (i) {
+var f1 = function (i):number {
   return i * i;
 };
 
 //change f4 to accept a string or a number as a parameter
-function f2(i) {
+function f2(i: (number|string)) {
   console.log("The value was " + i);
 }
 
 // Annotate the person objects with an interface. Some properties are optional
-var person1 = { name: "Bobby", move: function () {} };
-var person2 = { name: "Bobby", age: 42, move: function () {} };
+interface person{
+  name: string;
+  age?: number;
+  move: ()=>void;
+}
+var person1: person = { name: "Bobby", move: function () {} };
+var person2: person = { name: "Bobby", age: 42, move: function () {} };
 
 // Annotate mySearch to describe a function that accepts two string parameters
-var mySearch = function (src, sub) {
+interface search{
+  (arg1: string, arg2: string): boolean;
+}
+var mySearch: search = function (src, sub) {
   return src.search(sub) != -1;
 };
 
 //annotate the constructor arguments and properties
 class Point {
-  x = 0;
-  constructor(x, y = 0) {
+  x:number = 0;
+  y:number = 0;
+  constructor(x=0, y = 0) {
     this.x = x;
     this.y = y;
   }
@@ -45,16 +54,17 @@ class Point {
 }
 
 // annotate `PointPerson` to use all properties of `Person` and then add the missing required property
-class PointPerson {
+class PointPerson{
+  name: string;
   constructor(name) {
     this.name = name;
   }
 }
 
 // annotate this array
-var arrayOfAnyType = [1, "string", false];
+var arrayOfAnyType:any[] = [1, "string", false];
 
 // fix the foo object so bar and baz can be added
-let foo = {};
+let foo:any = {};
 foo.bar = 123;
 foo.baz = "hello world";
